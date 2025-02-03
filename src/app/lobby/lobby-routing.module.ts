f3175da3061dc56c3d377f133af5a18a48d56b45
Dilmurod Rozikov <drozikov@amcbridge.com>
2025-02-03 13:09:41 +0500

    done

--- src/app/lobby/lobby-routing.module.ts
+++ src/app/lobby/lobby-routing.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<<< HEAD:src/app/lobby/lobby-routing.module.ts
 import { StartMenuComponent } from './start-menu/start-menu.component';
-========
-import { MainMenuComponent } from './start-menu/start-menu.component';
->>>>>>>> d7af360fcb1811d5de9f4b2ffdc176364f050f2a:src/app/lobby/dashboard-routing.module.ts
 import { ChessBoardComponent } from './chess-board/chess-board.component';
 
 const routes: Routes = [
   {
     path: '',
     component: StartMenuComponent,
-    title: 'Main Menu',
+    title: 'Start Menu',
   },
   {
     path: 'board',
@@ -24,4 +20,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class DashboardRoutingModule {}
+export class LobbyRoutingModule {}
